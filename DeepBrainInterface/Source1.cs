36b858fbea38978d13d9121398837bf40d1c5ed7
ESIadmin <ESIadmin@ESI-NBROT017>
2024-02-21 12:11:44 +0100

    Add project files.

---
+++ DeepBrainInterface/Source1.cs
@@ -0,0 +1,20 @@
+﻿using Bonsai;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace DeepBrainInterface
+{
+    [Description("")]
+    [Combinator(MethodName = nameof(Generate))]
+    [WorkflowElementCategory(ElementCategory.Source)]
+    public class Source1
+    {
+        public IObservable<int> Generate()
+        {
+            return Observable.Return(0);
+        }
+    }
+}
