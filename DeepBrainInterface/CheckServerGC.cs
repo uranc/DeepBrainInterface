e598e1d2d309d20bda47b6dfafc91b0f693af47b
uranc <cemuran@gmail.com>
2026-03-06 02:06:41 +0100

    server GC test

---
+++ DeepBrainInterface/CheckServerGC.cs
@@ -0,0 +1,31 @@
+﻿using Bonsai;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Runtime;
+using System.ComponentModel;
+
+namespace DeepBrainInterface
+{
+    [Combinator]
+    [Description("Prints Server GC status to the console once, without altering the data stream.")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class PrintServerGC
+    {
+        public IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            bool printed = false;
+
+            return source.Do(x =>
+            {
+                if (!printed)
+                {
+                    Console.WriteLine("\n=========================================");
+                    Console.WriteLine("SERVER GC ENABLED: " + GCSettings.IsServerGC);
+                    Console.WriteLine("=========================================\n");
+                    printed = true;
+                }
+            });
+        }
+    }
+}
